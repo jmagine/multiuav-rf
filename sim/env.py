56b5127f654f6c1693ed02cbc244f2c954289391
jmagine <jasonma5501@gmail.com>
2020-04-06 11:29:11 -0700

    wip, started working with networkx

--- sim/env.py
+++ sim/env.py
@@ -8,10 +8,12 @@
 ---*-----------------------------------------------------------------------*'''
 
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 import random
 from scipy.spatial import Voronoi, voronoi_plot_2d, KDTree
 import time
+import networkx as nx
 
 import drone
 import bs
@@ -26,6 +28,8 @@ N_POI = 100
 P_BOUNDS = [[-1, 1], [-1, 1]]
 VIEW_BORDER = 0.1
 
+BANDWIDTH = 1.0
+
 plt.ion()
 
 class env():
@@ -36,8 +40,11 @@ class env():
     self.poi_active = []
     self.tx = {}
     self.bs = []
+    self.g = nx.Graph()
 
-    self.fig_vor = plt.figure()
+    self.plot = True
+
+    self.fig_vor = plt.figure(figsize=(8, 8), dpi=100)
     self.ax_vor = self.fig_vor.add_subplot(1,1,1)
     self.ax_vor.axis('equal')
     self.ax_vor.set_xlim([self.p_bounds[0][0] - VIEW_BORDER, self.p_bounds[0][1] + VIEW_BORDER])
@@ -47,16 +54,19 @@ class env():
     #self.net = 
 
   def setup(self):
+    self.bs = [bs.base_station([0,0])] #set up for multiple base stations in future work
+    for i, b in enumerate(self.bs):
+      self.g.add_node(i, p=b.pos)
+
     #generate or load in situation, including drone positions, pois, freqs
     for i in range(N_DRONES):
       x = random.uniform(self.p_bounds[0][0], self.p_bounds[0][1])
       y = random.uniform(self.p_bounds[1][0], self.p_bounds[1][1])
       self.drn.append(drone.drone(i, [x, y], 1))
+      self.g.add_node(len(self.bs) + i, p=self.drn[i].pos)
 
     for i in range(N_POI):
       self.poi.append(poi.poi([random.uniform(self.p_bounds[0][0], self.p_bounds[0][1]), random.uniform(self.p_bounds[1][0], self.p_bounds[1][1])], random.randint(0, 500), 500))
-
-    self.bs = [bs.base_station([0,0])] #set up for multiple base stations in future work
 
     #sort pois by start time
     self.poi.sort(key=lambda x: x.t_start)
@@ -65,11 +75,11 @@ class env():
     self.fig_vor.clf()
     ax = self.fig_vor.gca()
     
-    # Plot initial points
-    ax.plot([d.pos[0] for d in self.drn], [d.pos[1] for d in self.drn], 'b.')
+    # Plot drone points
+    ax.scatter([d.pos[0] for d in self.drn], [d.pos[1] for d in self.drn], marker='x', color='b')
     #print("initial",vor.filtered_points)
 
-    # Plot ridges points
+    # Plot ridge points
     #for region in vor.filtered_regions:
     #    vertices = vor.vertices[region, :]
     #    ax.plot(vertices[:, 0], vertices[:, 1], 'go')
@@ -77,13 +87,14 @@ class env():
     # Plot ridges
     for region in vor.filtered_regions:
         vertices = vor.vertices[region + [region[0]], :]
-        ax.plot(vertices[:, 0], vertices[:, 1], 'k-')
+        ax.plot(vertices[:, 0], vertices[:, 1], linewidth=1, linestyle='-', color='k')
 
     # Plot Centroids
     for region in vor.filtered_regions:
-      ax.plot(self.centroids[:, 0], self.centroids[:, 1], 'r.')
+      ax.scatter(self.centroids[:, 0], self.centroids[:, 1], marker='.', color='r')
 
   def tick(self):
+    t_start = time.time()
     #update positions of all drones
     for d in self.drn:
       d.tick()
@@ -92,26 +103,33 @@ class env():
 
     #evaluate data sent through network
 
-    #check for poi expiration #TODO actually maybe don't need to do this
+    #check for poi expiration if applicable
 
     #check for any new pois
     while len(self.poi) > 0 and self.t >= self.poi[0].t_start:
-      print(self.poi[0].t_start, self.t)
+      #print(self.poi[0].t_start, self.t)
       self.poi_active.append(self.poi[0])
       del self.poi[0] #TODO replace with something more efficient than this if slow
 
       #run assignment and network topology generation on new poi set
+    
+    #every few ticks, update voronoi partitioning
+    if self.t % 5 == 0:
       vor_points = [d.pos for d in self.drn] + [b.pos for b in self.bs]
       vor, self.centroids = self.cvt(vor_points)
       self.centroid_kdt = KDTree(self.centroids)
       #plot the voronoi partitioning
-      self.plot_vor(vor)
-      plt.show()
-      plt.pause(0.01)
 
       for d in self.drn:
         dist, idx = self.centroid_kdt.query(d.pos)
         d.set_pos_ref(self.centroid_kdt.data[idx])
+
+      if self.plot:
+        self.plot_vor(vor)
+        plt.show()
+        plt.pause(0.0001)
+
+      print('[tick] t: %.3f' % (time.time() - t_start))
 
     #print(self.poi)
     #print(self.poi_active)
@@ -195,15 +213,12 @@ class env():
       C_y *= (1.0 / (6.0 * A))
       centroids.append([C_x, C_y])
     
-    print(time.time() - t_start)#, centroids, len(vor.filtered_regions))
+    print("t: %d cvt t: %.3f" % (self.t, time.time() - t_start)) #, centroids, len(vor.filtered_regions))
     return vor, np.array(centroids)
 
   #evaluate cost function on a current snapshot of situation
+  #calculate capacities here
   def eval_snap(self):
-    pass
-
-  #draw and save a figure of the current situation
-  def plot(self):
     pass
 
   #print diagnostics for current situation
@@ -212,5 +227,25 @@ class env():
     print("[env] poi_active: %d" % (len(self.poi_active)))
     print("[env] tx: %d" % (len(self.tx.keys())))
 
-  
+  def capacity(self, freq, pos_tx, pos_rx, pw_tx=20, noise=-90):
+    noise_total = noise
 
+    pw_rx = power_fspl(pw_tx, freq, dist(pos_tx, pos_rx))
+
+    #cross interference
+    if tuple(freq) in self.tx:
+      for pos_noise in self.tx[tuple(freq)]:
+        noise_total += power_fspl(pw_tx, freq, dist(pos_noise, pos_rx))
+
+    return BANDWIDTH * math.log2(1 + pw_rx / noise_total)
+
+def dist(pos_0, pos_1):
+  return np.linalg.norm(np.array(pos_0) - np.array(pos_1))
+
+#calculate received signal power in dB
+def power_fspl(pw_tx, freq, dist):
+  if dist <= 0 or freq <= 0:
+    return pw_tx
+
+  loss = 20 * math.log10(4 * math.pi / 300 * freq * dist)
+  return pw_tx - loss
